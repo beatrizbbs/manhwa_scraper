e593901b0b08b5ae13b2f9a4f6fcb267d33f8830
Beatriz Braga <56659666+beatrizbbs@users.noreply.github.com>
2023-04-13 23:26:38 -0400

    Feat: Extracting summary and alternative titles
     - Getting summary of the manhwa
     - Create a function to get any information in summary content
     - Getting alternative titles

--- scraper.py
+++ scraper.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+import re
+
+def get_summary_item(item, soup):
+    content = soup.find("h5", string=re.compile(r".*" + item + r".*")).parent.find_next_sibling("div", class_="summary-content")
+    return content.text.strip()
+
 
 options = Options()
-options.add_argument('--headless')
-options.add_argument('--headless=new')
+options.add_argument("--headless")
+options.add_argument("--headless=new")
 
 dr = webdriver.Chrome(options=options)
 
@@ -13,9 +19,13 @@ dr.get("https://1stkissmanga.me/manga/i-dont-want-the-obssession-of-a-twisted-ar
 soup = BeautifulSoup(dr.page_source, "html5lib")
 
 title = soup.find("div", class_="post-title")
-image= soup.find("div", class_="summary_image").find("img").get('src')
+image = soup.find("div", class_="summary_image").find("img").get("src")
+summary = soup.find("div", class_="summary__content show-more")
+alternative = get_summary_item('Alternative', soup)
 
 print("Title: ", title.text.strip())
 print("Image: ", image)
+print("Summary: ", summary.text.strip())
+print("Alternative titles: ", alternative)
 
 dr.quit()
